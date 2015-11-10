73455ee6dc757f5e33b34996b61ff190424ff947
Hannes Pétur Eggertsson <hannespetur@gmail.com>
2015-11-10 10:42:00 +0000

    htsfile update

--- include/seqan/hts_io/hts_file.h
+++ include/seqan/hts_io/hts_file.h
@@ -16,21 +16,20 @@ class HtsFile
 {
   public:
     const char * filename;
-    htsFile * file_pointer;
+    htsFile * fp;
     bam_hdr_t * hdr;
     bam1_t * hts_record;
+    hts_idx_t * hts_index;
+    hts_itr_t * hts_iter = NULL;
 
     HtsFile(const char * filename)
         : filename(filename)
         {}
 };
 
-inline bool
-readRecord(HtsSequenceRecord & record, HtsFile & file)
+inline void
+_parseHtsSequenceRecord(HtsSequenceRecord & record, HtsFile & file)
 {
-    if (sam_read1(file.file_pointer, file.hdr, file.hts_record) < 0)
-        return false;
-
     record.qName = bam_get_qname(file.hts_record);
     int32_t lqseq = file.hts_record->core.l_qseq;
     resize(record.seq, lqseq);
@@ -40,28 +39,75 @@ readRecord(HtsSequenceRecord & record, HtsFile & file)
     {
       record.seq[i] = "=ACMGRSVTWYHKDBN"[bam_seqi(seqptr, i)];
     }
+}
+
+inline bool
+readRecord(HtsSequenceRecord & record, HtsFile & file)
+{
+    if (sam_read1(file.fp, file.hdr, file.hts_record) < 0)
+        return false;
+
+    _parseHtsSequenceRecord(record, file);
     return true;
 }
 
 inline void
 open(HtsFile & file)
 {
-    std::cout << "filename = " << file.filename << std::endl;
-    file.file_pointer = hts_open(file.filename, "r");
-    if (file.file_pointer == NULL)
+    file.fp = hts_open(file.filename, "r");
+    if (file.fp == NULL)
     {
         SEQAN_FAIL("Could not open file with filename %s", file.filename);
     }
     
-    file.hdr = sam_hdr_read(file.file_pointer);
+    file.hdr = sam_hdr_read(file.fp);
     file.hts_record = bam_init1();
 }
 
-// inline void
-// loadIndex(HtsFile & file)
-// {
-//     hts_idx_t * sam_index_load(file, const char *fn);
-// }
+inline bool
+loadIndex(HtsFile & file)
+{
+    file.hts_index = sam_index_load(file.fp, file.filename);
+    return file.hts_index != NULL;
+}
+
+inline bool
+loadIndex(HtsFile & file, bool build_if_unavailable)
+{
+    if (build_if_unavailable)
+    {
+        file.hts_index = sam_index_load(file.fp, file.filename);
+
+        if (file.hts_index == NULL)
+        {
+            bam_index_build(file.filename, 1000);
+            return loadIndex(file);
+        }
+
+        return true;
+    }
+
+    return loadIndex(file);
+}
+
+inline void
+setRegion(HtsFile & file, const char * region)
+{
+    if (file.hts_iter != NULL)
+        sam_itr_destroy(file.hts_iter);
+
+    file.hts_iter = sam_itr_querys(file.hts_index, file.hdr, region);
+}
+
+inline bool
+readRegion(HtsSequenceRecord & record, HtsFile & file)
+{
+    if (sam_itr_next(file.fp, file.hts_iter, file.hts_record) < 0)
+        return false;
+
+    _parseHtsSequenceRecord(record, file);
+    return true;
+}
 
 } // namespace seqan
 
